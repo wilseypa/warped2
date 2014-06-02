addc0e144e885a89fa12066c35dab4fbeeb5a74c
AJ Alt <aj@alt>
2014-06-02 14:49:10 -0400

    Fixed typo

--- src/SequentialEventDispatcher.cpp
+++ src/SequentialEventDispatcher.cpp
@@ -20,7 +20,7 @@ SequentialEventDispatcher::SequentialEventDispatcher(unsigned int max_sim_time,
 void SequentialEventDispatcher::startSimulation(
     const std::vector<std::vector<SimulationObject*>>& objects) {
     if (objects.size() != 1) {
-        throw std::runtime_error(std::string("Sequential simualtion only supports 1 partition."));
+        throw std::runtime_error(std::string("Sequential simulation only supports 1 partition."));
     }
 
     std::unordered_map<std::string, SimulationObject*> objects_by_name;
