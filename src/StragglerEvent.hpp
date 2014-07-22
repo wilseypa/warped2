84c178bf4ee645095e5bd2d6c872da8189c2098f
Sounak Gupta <guptask@mail.uc.edu>
2014-07-22 04:05:55 -0400

    updated the straggler event class by adding the sender name member variable and senderName() definition

--- src/StragglerEvent.hpp
+++ src/StragglerEvent.hpp
@@ -7,14 +7,20 @@ namespace warped {
 class StragglerEvent : public Event {
 public:
     StragglerEvent() = default;
-    StragglerEvent(const std::string& receiver_name, unsigned int timestamp)
-        : receiver_name_(receiver_name), timestamp_(timestamp)
+    StragglerEvent( const std::string& receiver_name, 
+                    const std::string& sender_name, 
+                    unsigned int timestamp )
+        :   receiver_name_(receiver_name), 
+            sender_name_(sender_name), 
+            timestamp_(timestamp)
     {}
 
     const std::string& receiverName() const { return receiver_name_; }
+    const std::string& senderName() const { return sender_name_; }
     unsigned int timestamp() const { return timestamp_; }
 
     std::string receiver_name_;
+    std::string sender_name_;
     unsigned int timestamp_;
     const std::vector<const std::unique_ptr<Event>> events_to_cancel_;
 };
