3c08ab24cd96558c47d4c720507b8e0c230b9a8f
Sounak Gupta <guptask@mail.uc.edu>
2014-09-30 03:07:28 -0400

    old unit tests for start scheduling, rollback and coast forwarding.

--- test/test_TimeWarpEventSet.cpp
+++ test/test_TimeWarpEventSet.cpp
@@ -18,31 +18,57 @@ TEST_CASE("Test the event set operations") {
     REQUIRE(twes.getEvent(0) == nullptr);
 
     SECTION("Insert event (+ve, -ve), get event and start scheduling events") {
+
         twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 15}));
         twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 10}));
         twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 11}));
         twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 10, false}));
         twes.insertEvent(1, std::shared_ptr<warped::Event>(new test_Event {"b", 9}));
         twes.insertEvent(1, std::shared_ptr<warped::Event>(new test_Event {"b", 9, false}));
+
         spe = twes.getEvent(0);
         REQUIRE(spe != nullptr);
         CHECK(spe->receiverName() == "b");
         CHECK(spe->timestamp() == 9);
         CHECK(spe->event_type_ == warped::EventType::POSITIVE);
+
         twes.replenishScheduler(1, spe);
+
         spe = twes.getEvent(0);
         REQUIRE(spe != nullptr);
         CHECK(spe->receiverName() == "b");
         CHECK(spe->timestamp() == 9);
         CHECK(spe->event_type_ == warped::EventType::NEGATIVE);
+
+        twes.rollback(1, 9);
+        auto list_obj1 = twes.getEventsForCoastForward(1);
+        CHECK(list_obj1->size() == 1);
+        CHECK((*list_obj1)[0]->timestamp() == 9);
+        CHECK((*list_obj1)[0]->event_type_ == warped::EventType::POSITIVE);
+
+        twes.startScheduling(1);
+
         spe = twes.getEvent(0);
         REQUIRE(spe != nullptr);
         CHECK(spe->receiverName() == "a");
         CHECK(spe->timestamp() == 15);
+
         twes.replenishScheduler(0, spe);
+
         spe = twes.getEvent(0);
         REQUIRE(spe != nullptr);
         CHECK(spe->receiverName() == "a");
         CHECK(spe->timestamp() == 11);
+
+        twes.replenishScheduler(0, spe);
+
+        spe = twes.getEvent(0);
+        REQUIRE(spe == nullptr);
+
+        twes.rollback(0, 10);
+        auto list_obj0 = twes.getEventsForCoastForward(0);
+        CHECK(list_obj0->size() == 2);
+        CHECK((*list_obj0)[0]->timestamp() == 11);
+        CHECK((*list_obj0)[1]->timestamp() == 15);
     }
 }
