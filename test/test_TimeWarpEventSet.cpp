1568bfd20d9dc91393fffbfe45f5fa36036bc73d
Sounak Gupta <guptask@mail.uc.edu>
2015-03-02 22:10:49 -0500

    added some more tests for event set.

--- test/test_TimeWarpEventSet.cpp
+++ test/test_TimeWarpEventSet.cpp
@@ -14,12 +14,30 @@ TEST_CASE("Test the event set operations") {
     unsigned int num_objects = 4, num_schedulers = 1, num_threads = 1;
     warped::TimeWarpEventSet twes;
     twes.initialize(num_objects, num_schedulers, num_threads);
-    std::shared_ptr<warped::Event> spe;
-    //REQUIRE(twes.getEvent(0) == nullptr);
+    std::shared_ptr<warped::Event> spe, restored_event;
+    REQUIRE(twes.getEvent(0) == nullptr);
 
     SECTION("Insert event (+ve, -ve), get event and start scheduling events") {
 
-        twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"c", 15}));
+        twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 5}));
+        twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 6}));
+
+        restored_event = twes.getEvent(0);
+        CHECK(restored_event != nullptr);
+        CHECK(restored_event->timestamp() == 5);
+        CHECK(restored_event->event_type_ == warped::EventType::POSITIVE);
+        twes.replenishScheduler(0);
+
+        spe = twes.getEvent(0);
+        CHECK(spe != nullptr);
+        CHECK(spe->timestamp() == 6);
+        CHECK(spe->event_type_ == warped::EventType::POSITIVE);
+        twes.replenishScheduler(0);
+
+        spe = twes.getEvent(0);
+        CHECK(spe == nullptr);
+
+        twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 16}));
         twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 10}));
         twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 15}));
         twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 10, false}));
@@ -29,70 +47,44 @@ TEST_CASE("Test the event set operations") {
         CHECK(spe->event_type_ == warped::EventType::NEGATIVE);
         twes.cancelEvent(0, spe);
         twes.startScheduling(0);
+
+        spe = twes.lastProcessedEvent(0);
+        CHECK(spe != nullptr);
+        CHECK(spe->timestamp() == 6);
+        CHECK(spe->event_type_ == warped::EventType::POSITIVE);
+
         spe = twes.getEvent(0);
         CHECK(spe != nullptr);
         CHECK(spe->timestamp() == 15);
         CHECK(spe->event_type_ == warped::EventType::POSITIVE);
+        twes.replenishScheduler(0);
 
-#if 0
-        twes.insertEvent(1, std::shared_ptr<warped::Event>(new test_Event {"b", 9}));
-        spe = std::shared_ptr<warped::Event>(new test_Event {"b", 9, false});
-        twes.insertEvent(1, spe);
-        spe = twes.getEvent(1);
-        twes. startScheduling(1, spe);
-        spe = twes.getEvent(1);
-        CHECK(spe == nullptr);
-
-        spe = twes.getEvent(0);
-        REQUIRE(spe != nullptr);
-        CHECK(spe->receiverName() == "a");
-        CHECK(spe->timestamp() == 9);
-        CHECK(spe->event_type_ == warped::EventType::POSITIVE);
-
-        twes.replenishScheduler(0, spe);
-
-        spe = twes.getEvent(0);
-        REQUIRE(spe != nullptr);
-        CHECK(spe->receiverName() == "b");
-        CHECK(spe->timestamp() == 9);
-        CHECK(spe->event_type_ == warped::EventType::POSITIVE);
-
-        twes.replenishScheduler(1, spe);
-
-        spe = twes.getEvent(0);
-        REQUIRE(spe != nullptr);
-        CHECK(spe->receiverName() == "b");
-        CHECK(spe->timestamp() == 9);
-        CHECK(spe->event_type_ == warped::EventType::NEGATIVE);
-
-        twes.rollback(1, 9);
-        auto list_obj1 = twes.getEventsForCoastForward(1);
-        CHECK(list_obj1->size() == 1);
-        CHECK((*list_obj1)[0]->timestamp() == 9);
-        CHECK((*list_obj1)[0]->event_type_ == warped::EventType::POSITIVE);
-
-        twes.startScheduling(1);
-
-        spe = twes.getEvent(0);
-        REQUIRE(spe == nullptr);
-
-        twes.replenishScheduler(0, spe);
-
-        spe = twes.getEvent(0);
-        REQUIRE(spe != nullptr);
-        CHECK(spe->receiverName() == "a");
+        spe = twes.lastProcessedEvent(0);
+        CHECK(spe != nullptr);
         CHECK(spe->timestamp() == 15);
+        CHECK(spe->event_type_ == warped::EventType::POSITIVE);
 
-        twes.replenishScheduler(0, spe);
+        twes.insertEvent(0, std::shared_ptr<warped::Event>(new test_Event {"a", 9}));
+        spe = twes.getEvent(0);
+        CHECK(spe != nullptr);
+        CHECK(spe->timestamp() == 9);
+        CHECK(spe->event_type_ == warped::EventType::POSITIVE);
+
+        twes.rollback(0, spe);
+        auto list_obj = twes.getEventsForCoastForward(0, spe, restored_event);
+        CHECK(list_obj->size() == 1);
+        CHECK((*list_obj)[0]->timestamp() == 6);
+        twes.startScheduling(0);
 
         spe = twes.getEvent(0);
-        REQUIRE(spe == nullptr);
+        CHECK(spe != nullptr);
+        CHECK(spe->timestamp() == 9);
+        CHECK(spe->event_type_ == warped::EventType::POSITIVE);
+        twes.replenishScheduler(0);
 
-        twes.rollback(0, 10);
-        auto list_obj0 = twes.getEventsForCoastForward(0);
-        CHECK(list_obj0->size() == 2);
-        CHECK((*list_obj0)[0]->timestamp() == 11);
-        CHECK((*list_obj0)[1]->timestamp() == 15);
-#endif
+        spe = twes.getEvent(0);
+        CHECK(spe != nullptr);
+        CHECK(spe->timestamp() == 15);
+        CHECK(spe->event_type_ == warped::EventType::POSITIVE);
     }
 }
