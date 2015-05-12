ab6c48ea12107e392d220f14e770006ffc5756e8
Sounak Gupta <guptask@mail.uc.edu>
2015-05-12 12:18:59 -0400

    ladder erase and recurse rung

--- src/LadderQueue.cpp
+++ src/LadderQueue.cpp
@@ -72,9 +72,7 @@ std::shared_ptr<Event> LadderQueue::begin() {
     /* Move from top to top of empty ladder */
     /* Check if failed to create the first rung */
     bool is_bucket_width_static = false;
-    if (!createNewRung(top_.size(), min_ts_, &is_bucket_width_static)) {
-        assert(0);
-    }
+    if (!createNewRung(top_.size(), min_ts_, &is_bucket_width_static)) assert(0);
 
     /* Transfer events from Top to 1st rung of Ladder */
     /* Note: No need to update rCur[0] since it will be equal to rStart[0] initially. */
@@ -131,7 +129,72 @@ std::shared_ptr<Event> LadderQueue::begin() {
 
 void LadderQueue::erase(std::shared_ptr<Event> event) {
 
-    std::cout << event->timestamp();
+    assert(event != nullptr);
+    auto timestamp = event->timestamp();
+
+    /* Check and erase from top, if found */
+    if (timestamp > top_start_) {
+        if (top_.empty()) assert(0);
+        for (auto iter = top_.begin(); iter != top_.end(); iter++) {
+            if (iter == top_.end()) assert(0);
+            if (*iter == event) {
+                (void) top_.erase(iter);
+                break;
+            }
+        }
+        return;
+    }
+
+    /* Step through rungs */
+    unsigned int rung_index = 0;
+    for (rung_index = 0; rung_index < n_rung_; rung_index++) {
+        if (timestamp >= r_current_[rung_index]) break;
+    }
+
+    if (rung_index < n_rung_) {  /* found a rung */
+        unsigned int bucket_index = std::min(
+            (unsigned int)(timestamp - r_start_[rung_index]) / bucket_width_[rung_index],
+                                                            RUNG_BUCKET_CNT(rung_index)-1);
+        auto rung_bucket = rung_[rung_index][bucket_index];
+        if (!rung_bucket->empty()) {
+            for (auto iter = rung_bucket->begin(); ; iter++) {
+                if (iter == rung_bucket->end()) assert(0);
+                if ((*iter) == event) {
+                    (void) rung_bucket->erase(iter);
+                    break;
+                }
+            }
+
+            /* If bucket is empty after deletion */
+            if (rung_bucket->empty()) {
+                /* Check whether rung bucket count needs adjustment */
+                if (rung_bucket_cnt_[rung_index] == bucket_index+1) {
+                    bool is_rung_empty = false;
+                    do {
+                        if (!bucket_index) {
+                            is_rung_empty = true;
+                            rung_bucket_cnt_[rung_index] = 0;
+                            r_current_[rung_index] = r_start_[rung_index];
+                            break;
+                        }
+                        bucket_index--;
+                        rung_bucket = rung_[rung_index][bucket_index];
+                    } while (rung_bucket->empty());
+
+                    if (!is_rung_empty) {
+                        rung_bucket_cnt_[rung_index] = bucket_index+1;
+                    }
+                }
+            }
+        } else {
+            assert(0);
+        }
+        return;
+    }
+
+    /* Check and erase from bottom, if present */
+    if (bottom_.empty()) assert(0);
+    (void) bottom_.erase(event);
 }
 
 void LadderQueue::insert(std::shared_ptr<Event> event) {
@@ -243,10 +306,52 @@ bool LadderQueue::createRungForBottomTransfer(unsigned int start_val) {
     return true;
 }
 
-bool LadderQueue::recurseRung( unsigned int *index ) {
+bool LadderQueue::recurseRung(unsigned int *index) {
 
-    std::cout << *index;
-    return true;
+    bool status = false;
+    *index = 0;
+
+    /* Find_bucket label */
+    while(1) {
+
+        if (!n_rung_) {
+            status = false;
+            break;
+        }
+        if (n_rung_ > MAX_RUNG_CNT) assert(0);
+
+        unsigned int bucket_index = 0;
+        r_current_[n_rung_-1] = r_start_[n_rung_-1];
+        while ((bucket_index < RUNG_BUCKET_CNT(n_rung_-1)) && 
+                            rung_[n_rung_-1][bucket_index]->empty()) {
+            bucket_index++;
+            r_current_[n_rung_-1] += bucket_width_[n_rung_-1];
+        }
+
+        if (bucket_index == RUNG_BUCKET_CNT(n_rung_-1)) {
+            r_start_[n_rung_-1]         = 0;
+            r_current_[n_rung_-1]       = 0;
+            bucket_width_[n_rung_-1]    = 0;
+            rung_bucket_cnt_[n_rung_-1] = 0;
+            n_rung_--;
+
+        } else {
+            if (rung_[n_rung_-1][bucket_index]->size() > THRESHOLD) {
+                bool is_bucket_width_static = false;
+                if (!createNewRung( rung_[n_rung_-1][bucket_index]->size(),
+                                    r_current_[n_rung_-1], 
+                                    &is_bucket_width_static )) {
+                    if (is_bucket_width_static) {
+                        status = true;
+                    }
+                    break;
+                }
+            }
+            //TODO
+        }
+    }
+
+    return status;
 }
 
 } // namespace warped
