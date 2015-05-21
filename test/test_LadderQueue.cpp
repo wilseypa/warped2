8d08661b6edd6db72be0a99f18a7d27ab8133239
Sounak Gupta <guptask@mail.uc.edu>
2015-05-21 03:46:39 -0400

    test events from different objects but same timestamp. insertion order randomized.

--- test/test_LadderQueue.cpp
+++ test/test_LadderQueue.cpp
@@ -14,7 +14,7 @@ TEST_CASE("Ladder Queue operations") {
     std::shared_ptr<warped::Event> e;
     REQUIRE(q.begin() == nullptr);
 
-    SECTION("Adding events individually") {
+    SECTION("Insert, read and erase event on a Laddder Queue") {
 
         // Basic check for insert(), begin() and erase()
         q.insert(std::shared_ptr<warped::Event>(new test_Event {"1", 10}));
@@ -70,55 +70,27 @@ TEST_CASE("Ladder Queue operations") {
         REQUIRE(e == nullptr);
     }
 
-#if 0
-    SECTION("Adding events in a vector") {
-        std::vector<std::shared_ptr<warped::Event>> v;
-        v.emplace_back(new test_Event {"1", 10});
-        v.emplace_back(new test_Event {"2", 5});
-        v.emplace_back(new test_Event {"3", 15});
+    SECTION("Test events with same timestamp but different insertion order") {
 
-        q.push(std::move(v));
-        REQUIRE(!q.empty());
-        REQUIRE(q.size() == 3);
-        REQUIRE(q.peek() != nullptr);
-
-        // pop all three events
-        int expected_timestamps[3] = {5, 10, 15};
-
-        for (int i = 0; i < 3; ++i) {
-            spe = q.pop();
-            CHECK(q.size() == (2 - i));
-            REQUIRE(spe != nullptr);
-            CHECK(spe->timestamp() == expected_timestamps[i]);
-        }
-
-        REQUIRE(q.peek() == nullptr);
-        REQUIRE(q.empty());
+        q.insert(std::shared_ptr<warped::Event>(new test_Event {"3", 10}));
+        q.insert(std::shared_ptr<warped::Event>(new test_Event {"1", 10}));
+        q.insert(std::shared_ptr<warped::Event>(new test_Event {"2", 10}));
+        e = q.begin();
+        REQUIRE(e != nullptr);
+        CHECK(e->receiverName() == "1");
+        CHECK(e->timestamp() == 10);
+        //q.erase(e);
+        //e = q.begin();
+        //REQUIRE(e != nullptr);
+        //CHECK(e->receiverName() == "2");
+        //CHECK(e->timestamp() == 10);
+        //q.erase(e);
+        //e = q.begin();
+        //REQUIRE(e != nullptr);
+        //CHECK(e->receiverName() == "3");
+        //CHECK(e->timestamp() == 10);
+        //q.erase(e);
+        //e = q.begin();
+        //REQUIRE(e == nullptr);
     }
-
-    SECTION("Adding multiple events with the same timestamp") {
-        std::vector<std::shared_ptr<warped::Event>> v;
-        v.emplace_back(new test_Event {"1", 10});
-        v.emplace_back(new test_Event {"2", 5});
-        v.emplace_back(new test_Event {"3", 10});
-
-        q.push(std::move(v));
-        REQUIRE(!q.empty());
-        REQUIRE(q.size() == 3);
-        REQUIRE(q.peek() != nullptr);
-
-        // pop all three events
-        int expected_timestamps[3] = {5, 10, 10};
-
-        for (int i = 0; i < 3; ++i) {
-            spe = q.pop();
-            CHECK(q.size() == (2 - i));
-            REQUIRE(spe != nullptr);
-            CHECK(spe->timestamp() == expected_timestamps[i]);
-        }
-
-        REQUIRE(q.peek() == nullptr);
-        REQUIRE(q.empty());
-    }
-#endif
 }
