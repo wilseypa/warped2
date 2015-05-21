985e63ae90ae8d329e8dfd9970dee4046ba6f484
Sounak Gupta <guptask@mail.uc.edu>
2015-05-21 03:01:30 -0400

    added ladder queue unit tests for event chain

--- test/test_LadderQueue.cpp
+++ test/test_LadderQueue.cpp
@@ -15,6 +15,8 @@ TEST_CASE("Ladder Queue operations") {
     REQUIRE(q.begin() == nullptr);
 
     SECTION("Adding events individually") {
+
+        // Basic check for insert(), begin() and erase()
         q.insert(std::shared_ptr<warped::Event>(new test_Event {"1", 10}));
         e = q.begin();
         REQUIRE(e != nullptr);
@@ -24,41 +26,51 @@ TEST_CASE("Ladder Queue operations") {
         e = q.begin();
         REQUIRE(e == nullptr);
 
-#if 0
-        // Add a second event with a smaller timestamp
-        q.push(std::shared_ptr<warped::Event>(new test_Event {"2", 5}));
-        CHECK(q.size() == 2);
-        REQUIRE(q.peek() != nullptr);
+        // Check for algorithmic accuracy
+        // Add events in decreasing order
+        q.insert(std::shared_ptr<warped::Event>(new test_Event {"1", 10}));
+        q.insert(std::shared_ptr<warped::Event>(new test_Event {"2", 5}));
+        e = q.begin();
+        REQUIRE(e != nullptr);
+        CHECK(e->receiverName() == "2");
+        CHECK(e->timestamp() == 5);
+        q.erase(e);
+        e = q.begin();
+        REQUIRE(e != nullptr);
+        CHECK(e->receiverName() == "1");
+        CHECK(e->timestamp() == 10);
 
-        spe = q.peek();
-        REQUIRE(spe != nullptr);
-        CHECK(spe->receiverName() == "2");
-        CHECK(spe->timestamp() == 5);
+        // Add event with a larger timestamp
+        q.insert(std::shared_ptr<warped::Event>(new test_Event {"3", 15}));
+        e = q.begin();
+        REQUIRE(e != nullptr);
+        CHECK(e->receiverName() == "1");
+        CHECK(e->timestamp() == 10);
 
-        // Add a third event with a larger timestamp
-        q.push(std::shared_ptr<warped::Event>(new test_Event {"3", 15}));
-        CHECK(q.size() == 3);
-        REQUIRE(q.peek() != nullptr);
+        // Add event with a smaller timestamp
+        q.insert(std::shared_ptr<warped::Event>(new test_Event {"3", 8}));
+        e = q.begin();
+        REQUIRE(e != nullptr);
+        CHECK(e->receiverName() == "3");
+        CHECK(e->timestamp() == 8);
 
-        spe = q.peek();
-        REQUIRE(spe != nullptr);
-        CHECK(spe->receiverName() == "2");
-        CHECK(spe->timestamp() == 5);
-
-        // pop all three events
-        int expected_timestamps[3] = {5, 10, 15};
-
-       for (int i = 0; i < 3; ++i) {
-            spe = q.pop();
-            CHECK(q.size() == (2 - i));
-            REQUIRE(spe != nullptr);
-            CHECK(spe->timestamp() == expected_timestamps[i]);
-        }
-
-        REQUIRE(q.peek() == nullptr);
-        REQUIRE(q.empty());
+        // Delete the 3 events
+        q.erase(e);
+        e = q.begin();
+        REQUIRE(e != nullptr);
+        CHECK(e->receiverName() == "1");
+        CHECK(e->timestamp() == 10);
+        q.erase(e);
+        e = q.begin();
+        REQUIRE(e != nullptr);
+        CHECK(e->receiverName() == "3");
+        CHECK(e->timestamp() == 15);
+        q.erase(e);
+        e = q.begin();
+        REQUIRE(e == nullptr);
     }
 
+#if 0
     SECTION("Adding events in a vector") {
         std::vector<std::shared_ptr<warped::Event>> v;
         v.emplace_back(new test_Event {"1", 10});
@@ -107,6 +119,6 @@ TEST_CASE("Ladder Queue operations") {
 
         REQUIRE(q.peek() == nullptr);
         REQUIRE(q.empty());
-#endif
     }
+#endif
 }
