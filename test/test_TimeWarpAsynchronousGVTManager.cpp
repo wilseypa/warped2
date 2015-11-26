dff5e66538ef985be8acd6ca82ecaa996315cb83
Doug Weber <weberd6@mail.uc.edu>
2015-11-25 23:01:22 -0500

    Update tests for GVT changes

--- test/test_TimeWarpAsynchronousGVTManager.cpp
+++ test/test_TimeWarpAsynchronousGVTManager.cpp
@@ -4,7 +4,7 @@
 #include <memory>
 #include <sstream>
 
-#include "TimeWarpMatternGVTManager.hpp"
+#include "TimeWarpAsynchronousGVTManager.hpp"
 #include "serialization.hpp"
 #include "utility/memory.hpp"
 
