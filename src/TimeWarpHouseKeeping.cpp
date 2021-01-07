4001d42def5aeb996778ce8c69c2a0c8dd39f6e4
meintega <griffinmeintel@gmail.com>
2021-01-06 19:52:02 -0500

    Adding the template files to be filled in for the HouseKeeping Class

---
+++ src/TimeWarpHouseKeeping.cpp
@@ -0,0 +1,16 @@
+/* 
+----notes----
+Class houseKeeping() {
+    gvt.barrier() <- # of worker threads + 2 (Coordinate the worker threads w/ gCollect and receiveEvent) 
+    gvtCntrl.barrier() <- 2 (Sync gvtCntrl w/ fCollect) ==> Figure 3
+
+    gvt.gvtEstCycle <- false
+    gvt.gvtCycleInterval <- delay (delay > 0: Time between gvt estimation cycles)
+    gvt.gvt <- 0
+    gvt.nextGvt <- 0
+
+    gvtCntrl **thread** (GVT Estimation Task)
+    receiveEvent **thread**
+    fCollect **thread**
+}
+*/
