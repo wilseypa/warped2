3073b204fc6827c4a2bec5d5fa387699714cc7ea
Jack Johnson <jackvj98@outlook.com>
2021-01-07 15:28:15 -0500

    Added the file pair that will host the gvtCntrl thread.

---
+++ src/TimeWarpGvtCntrl.cpp
@@ -0,0 +1,23 @@
+
+#include "utility/warnings.hpp"
+#include "utility/memory.hpp"   
+
+namespace warped
+{
+    class GvtCntrl
+    {
+        public:
+            void GvtCntrl::initialize()
+            {
+
+            }
+
+                        
+            void GvtCntrl::barrier()
+            {
+
+            }
+
+
+    };
+}
---
+++ src/TimeWarpGvtCntrl.hpp
@@ -0,0 +1,22 @@
+// Note this file pair will house the gvtcntrl thread.
+#include <memory> // for shared_ptr
+#include <atomic>
+#include <shared_mutex>
+
+namespace warped
+{
+    class GvtCntrl
+    {
+        public:
+            void GvtCntrl::initialize()
+            {
+
+            }
+            
+            void GvtCntrl::barrier()
+            {
+
+            }
+
+    };
+}
