5b11a82d418ffe6507eb2843efd966c3304f5469
Griffin Meintel <griffinmeintel@gmail.com>
2021-06-03 15:38:37 -0400

    Cleaning up receiveEvent notes now that psuedo code isn't needed

--- src/receiveEvent.cpp
+++ src/receiveEvent.cpp
@@ -1,24 +1,3 @@
-/*
-
-one instance for each cluster node.
-takes in all event msgs on the MPI EVENT comm
-
-receiveEvent thread:
-
-while true do
-    m <- MPI_Recv(EVENT, MPI_ANY_SOURCE) // receive all events, set them to m
-
-    if m.tag = "Verify_Idle" then
-        MPI_Barrier(MSGs_Proc)
-    else
-        lp <- findLpPtr(m.lp) // find the ptr for the given lp
-        lp.inQ.insert(m.e) //use that ptr to insert the event into the queue
-    end if
-end while
-
-end thread loop
-
-*/
 #include "receiveEvent.hpp"
 
 namespace warped {
