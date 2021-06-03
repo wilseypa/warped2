f00ad45c45d5dd7624ea036abe41a1c49d027bb7
Griffin Meintel <griffinmeintel@gmail.com>
2021-06-03 15:08:21 -0400

    Consolidating changes that Jack and I made in separate files to one receiveEvent file set

--- src/receiveEvent.cpp
+++ src/receiveEvent.cpp
@@ -22,7 +22,27 @@ end thread loop
 #include "receiveEvent.hpp"
 
 namespace warped {
-    receiveEvent::receiveEvent() {}
+    receiveEvent::receiveEvent(
+    unsigned int num_worker_threads,
+    bool is_lp_migration_on,
+    unsigned int num_refresh_per_gvt,
+    unsigned int num_events_per_refresh,
+    std::shared_ptr<TimeWarpCommunicationManager> comm_manager,
+    std::unique_ptr<TimeWarpEventSet> event_set,
+    std::unique_ptr<TimeWarpGVTManager> gvt_manager,
+    std::unique_ptr<TimeWarpStateManager> state_manager,
+    std::unique_ptr<TimeWarpOutputManager> output_manager,
+    std::unique_ptr<TimeWarpFileStreamManager> twfs_manager,
+    std::unique_ptr<TimeWarpTerminationManager> termination_manager,
+    std::unique_ptr<TimeWarpEventDispatcher> event_dispatcher,
+    std::unique_ptr<TimeWarpStatistics> tw_stats) :
+        num_worker_threads_(num_worker_threads),
+        is_lp_migration_on_(is_lp_migration_on),
+        num_refresh_per_gvt_(num_refresh_per_gvt), num_events_per_refresh_(num_events_per_refresh), 
+        comm_manager_(comm_manager), event_set_(std::move(event_set)), 
+        gvt_manager_(std::move(gvt_manager)), state_manager_(std::move(state_manager)),
+        output_manager_(std::move(output_manager)), twfs_manager_(std::move(twfs_manager)),
+        termination_manager_(std::move(termination_manager)), tw_stats_(std::move(tw_stats)) {}
 
     void receiveEvent::initialize() 
     {
--- src/receiveEvent.hpp
+++ src/receiveEvent.hpp
@@ -8,13 +8,26 @@ namespace warped
     {
         public:
 
-            receiveEvent::receiveEvent();
+            receiveEvent::receiveEvent(
+            unsigned int num_worker_threads,
+            bool is_lp_migration_on,
+            unsigned int num_refresh_per_gvt,
+            unsigned int num_events_per_refresh,
+            std::shared_ptr<TimeWarpCommunicationManager> comm_manager,
+            std::unique_ptr<TimeWarpEventSet> event_set,
+            std::unique_ptr<TimeWarpGVTManager> gvt_manager,
+            std::unique_ptr<TimeWarpStateManager> state_manager,
+            std::unique_ptr<TimeWarpOutputManager> output_manager,
+            std::unique_ptr<TimeWarpFileStreamManager> twfs_manager,
+            std::unique_ptr<TimeWarpTerminationManager> termination_manager,
+            std::unique_ptr<TimeWarpEventDispatcher> event_dispatcher,
+            std::unique_ptr<TimeWarpStatistics> tw_stats);
 
-            void receiveEvent::thread();
             void receiveEvent::initialize();
 
+            void receiveEvent::thread();
+            
         private:
-
 
             const std::shared_ptr<TimeWarpMPICommunicationManager> comm_manager_;
             const std::unique_ptr<TimeWarpEventSet> event_set_;
