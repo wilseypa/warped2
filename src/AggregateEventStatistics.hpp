6d29c83cac7915ec22890cfe082eeef9d454e08c
gourav-shinde <gauravsanjayshinde@gmail.com>
2023-07-20 19:51:47 -0400

    added bitset to event

--- src/AggregateEventStatistics.hpp
+++ src/AggregateEventStatistics.hpp
@@ -8,41 +8,47 @@
 
 #include "EventStatistics.hpp"
 
-namespace warped {
+namespace warped
+{
 
-class Event;
+    class Event;
 
-// Collects a count of events sent between objects. The objects and events are
-// represented as an undirected graph with weighted edges such that the weight
-// of an edge is the count of events sent between objects incident that edge.
-class AggregateEventStatistics : public EventStatistics {
-public:
-    enum class OutputType {Graphviz, Metis};
-    typedef std::pair<std::string, std::string> Edge;
-    typedef unsigned int stat_type;
+    // Collects a count of events sent between objects. The objects and events are
+    // represented as an undirected graph with weighted edges such that the weight
+    // of an edge is the count of events sent between objects incident that edge.
+    class AggregateEventStatistics : public EventStatistics
+    {
+    public:
+        enum class OutputType
+        {
+            Graphviz,
+            Metis
+        };
+        typedef std::pair<std::string, std::string> Edge;
+        typedef unsigned int stat_type;
 
-    AggregateEventStatistics(std::string filename, OutputType output_type);
+        AggregateEventStatistics(std::string filename, OutputType output_type);
 
-    void record(const std::string& source, unsigned int send_time, Event* event);
-    // Record zero or more events at once
-    void record(const std::string& source, unsigned int send_time,
-                        const std::vector<std::shared_ptr<Event>>& events);
+        void record(const std::string &source, unsigned int send_time, Event *event);
+        // Record zero or more events at once
+        void record(const std::string &source, unsigned int send_time,
+                    const std::vector<std::shared_ptr<Event>> &events);
 
-    // Make an undirectional edge connecting two vertices.
-    Edge makeEdge(const std::string& a, const std::string& b) const;
+        // Make an undirectional edge connecting two vertices.
+        Edge makeEdge(const std::string &a, const std::string &b) const;
 
-    // Return the count of events along the edge e.
-    unsigned int getStat(const Edge& e) const;
+        // Return the count of events along the edge e.
+        unsigned int getStat(const Edge &e) const;
 
-private:
-    OutputType output_type_;
-    std::map<Edge, stat_type> edge_stats_;
+    private:
+        OutputType output_type_;
+        std::map<Edge, stat_type> edge_stats_;
 
-    // Print stats by delegating to the different output formats
-    std::ostream& printStats(std::ostream& stream) const;
-    std::ostream& printGraphviz(std::ostream& stream) const;
-    std::ostream& printMetis(std::ostream& stream) const;
-};
+        // Print stats by delegating to the different output formats
+        std::ostream &printStats(std::ostream &stream) const;
+        std::ostream &printGraphviz(std::ostream &stream) const;
+        std::ostream &printMetis(std::ostream &stream) const;
+    };
 
 } // namespace warped
 
